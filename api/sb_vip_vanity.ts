d18636852b3862cc4f34fba105e7de673c5a5752
caneleex <git@mlnr.dev>
2022-04-12 21:02:36 +0200

    reorder public id check

--- api/sb_vip_vanity.ts
+++ api/sb_vip_vanity.ts
@@ -1,15 +1,27 @@
 import { handler } from '../lib/handler';
-import { followup, get_string_option } from '../lib/utils';
+import { followup, get_string_option, respond } from '../lib/utils';
 import { CFMetadataResponse } from '../lib/types'
 import fetch from 'node-fetch';
 import FormData from 'form-data'
-import { APIChatInputApplicationCommandInteraction, MessageFlags, RESTPostAPIInteractionCallbackJSONBody, RouteBases, Routes } from 'discord-api-types/v10';
+import { APIChatInputApplicationCommandInteraction, InteractionResponseType, MessageFlags, RESTPostAPIInteractionCallbackJSONBody, RouteBases, Routes } from 'discord-api-types/v10';
 
 const metadata_api_url = (key: string) => `https://api.cloudflare.com/client/v4/accounts/6dccc8a823380a32fe8792904b2cd886/storage/kv/namespaces/b44b93e4cc174443aca099a3763b29ff/metadata/${key}`
 const value_api_url = (key: string) => `https://api.cloudflare.com/client/v4/accounts/6dccc8a823380a32fe8792904b2cd886/storage/kv/namespaces/b44b93e4cc174443aca099a3763b29ff/values/${key}`
 const public_id_regex = new RegExp(/^[a-f0-9]{64}$/);
 
 export default handler(async (interaction, res) => {
+  const slash_command = interaction as APIChatInputApplicationCommandInteraction
+  const sb_user_id = get_string_option(slash_command, 'sb_user_id')
+  if (!public_id_regex.test(sb_user_id)) {
+    respond({
+      type: InteractionResponseType.ChannelMessageWithSource,
+      data: {
+        content: 'Provided user id is not a valid public user id.',
+        flags: MessageFlags.Ephemeral
+      }
+    }, res)
+    return
+  }
   await fetch(`${RouteBases.api}${Routes.interactionCallback(interaction.id, interaction.token)}`, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
@@ -20,7 +32,6 @@ export default handler(async (interaction, res) => {
       }
     } as RESTPostAPIInteractionCallbackJSONBody)
   })
-  const slash_command = interaction as APIChatInputApplicationCommandInteraction
   const vanity = get_string_option(slash_command, 'vanity').toLowerCase()
   const metadata_response = await fetch(metadata_api_url(vanity), {
     method: 'GET',
@@ -35,13 +46,6 @@ export default handler(async (interaction, res) => {
       }, interaction, res)
       return
     }
-  }
-  const sb_user_id = get_string_option(slash_command, 'sb_user_id')
-  if (!public_id_regex.test(sb_user_id)) {
-    followup({
-      content: 'Provided user id is not a valid public user id.',
-    }, interaction, res)
-    return
   }
   const form_data = new FormData()
   form_data.append('value', sb_user_id)
