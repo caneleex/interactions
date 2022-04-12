f29a11241e22b5c8b2343a951a7784101a7ce1cd
caneleex <git@mlnr.dev>
2022-04-12 20:57:11 +0200

    remove pointless role check

--- api/sb_vip_vanity.ts
+++ api/sb_vip_vanity.ts
@@ -1,27 +1,15 @@
 import { handler } from '../lib/handler';
-import { followup, get_string_option, respond } from '../lib/utils';
+import { followup, get_string_option } from '../lib/utils';
 import { CFMetadataResponse } from '../lib/types'
 import fetch from 'node-fetch';
 import FormData from 'form-data'
-import { APIChatInputApplicationCommandInteraction, InteractionResponseType, MessageFlags, RESTPostAPIInteractionCallbackJSONBody, RouteBases, Routes } from 'discord-api-types/v10';
+import { APIChatInputApplicationCommandInteraction, MessageFlags, RESTPostAPIInteractionCallbackJSONBody, RouteBases, Routes } from 'discord-api-types/v10';
 
 const metadata_api_url = (key: string) => `https://api.cloudflare.com/client/v4/accounts/6dccc8a823380a32fe8792904b2cd886/storage/kv/namespaces/b44b93e4cc174443aca099a3763b29ff/metadata/${key}`
 const value_api_url = (key: string) => `https://api.cloudflare.com/client/v4/accounts/6dccc8a823380a32fe8792904b2cd886/storage/kv/namespaces/b44b93e4cc174443aca099a3763b29ff/values/${key}`
 const public_id_regex = new RegExp(/^[a-f0-9]{64}$/);
-const vip_role_id = '755511470305050715'
 
 export default handler(async (interaction, res) => {
-  const member = interaction.member
-  if (!member.roles.includes(vip_role_id)) {
-    respond({
-      type: InteractionResponseType.ChannelMessageWithSource,
-      data: {
-        content: 'This command is VIP only.',
-        flags: MessageFlags.Ephemeral
-      }
-    }, res)
-    return
-  }
   await fetch(`${RouteBases.api}${Routes.interactionCallback(interaction.id, interaction.token)}`, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
@@ -38,7 +26,7 @@ export default handler(async (interaction, res) => {
     method: 'GET',
     headers: {'Authorization': process.env.CF_TOKEN}
   })
-  const user_id = member.user.id
+  const user_id = interaction.member.user.id
   if (metadata_response.ok) {
     const cf_response = await metadata_response.json() as CFMetadataResponse
     if (cf_response.result.id !== user_id) {
